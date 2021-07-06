8e9a3aa781b7513636b14824fbad805147ee8efa
Klim Nikishin <klimnikishinofficial@gmail.com>
2021-07-06 20:09:34 +0500

    Initial Commit

---
+++ PureMind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PureMind
+//
+//  Created by Клим on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
